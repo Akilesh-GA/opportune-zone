bd616bd1a7640e71f73139371fe031e32a46a576
Akilesh-GA <akileshga.1466@gmail.com>
2025-08-03 11:55:12 +0530

    feat : spring boot updated

--- backend/src/main/java/com/zidio/backend/BackendApplication.java
+++ backend/src/main/java/com/zidio/backend/BackendApplication.java
@@ -2,12 +2,24 @@ package com.zidio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Opportune Zone!";
+    }
+
+    @GetMapping("/port")
+    public String hello() {
+        return "Hello from Spring Boot on port 8084!";
+    }
 }
--- backend/src/main/java/com/zidio/backend/TestController.java
+++ backend/src/main/java/com/zidio/backend/TestController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/")
-    public void home() {
-        System.out.println("Welcome to Opportune Zone");
+    @GetMapping("/test")
+    public String home() {
+        return "Welcome to Opportune Zone Test Controller";
     }
 }
