bddd3025af2a3a2895313b70754443b868ceff19
Akilesh-GA <akileshga.1466@gmail.com>
2025-08-15 23:09:05 +0530

    feats : pages updated

---
+++ backend/src/main/java/com/zidio/backend/AboutUs.java
@@ -0,0 +1,12 @@
+package com.zidio.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutUs {
+    @GetMapping("/aboutus")
+    public String aboutUs() {
+        return "redirect:/aboutus.html";
+    }
+}
